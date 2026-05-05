f40c29fd3e13362286b929aa9ffd5fbc36cec4f1
Findoku <117414599+Findoku@users.noreply.github.com>
2026-05-05 11:40:17 -0500

    Update morsePho.py

--- morsePho.py
+++ morsePho.py
@@ -4,24 +4,24 @@ import clipboard
 import photos
 
 class Extracter(ui.View):
-	def __init__(self):
-	
-		# Some UI Elements
-		#(...)
-		
-		# Take Photo Button
-		self.take_photo = ui.Button(flex = 'LR', title = 'Take Photo')
-		self.take_photo.action = self.take_photo_action
-		# Some Button Styles
-		#(...)
-		self.add_subview(self.take_photo)
-		
-	# Take Photo Action
-	@ui.in_background
-	def take_photo_action(self, sender):
-		image = photos.capture_image()
-		photos.save_image(image)
-		
-if __name__=='__main__':
-	view = Extracter()
-	view.present('sheet')
+    def __init__(self):
+        # Take Photo Button
+        self.take_photo_btn = ui.Button(flex='LR', title='Take Photo')
+        self.take_photo_btn.action = self.take_photo_action
+        self.add_subview(self.take_photo_btn)
+
+    @ui.in_background
+    def take_photo_action(self, sender):
+        image = photos.capture_image()
+        
+        # Check if image is None before doing anything
+        if image is None:
+            console.alert('No photo taken', 'Please take a photo before saving.', 'OK', hide_cancel_button=True)
+            return
+        
+        photos.save_image(image)
+        console.hud_alert('Photo saved!', 'success', 1.0)
+
+if __name__ == '__main__':
+    view = Extracter()
+    view.present('sheet')
